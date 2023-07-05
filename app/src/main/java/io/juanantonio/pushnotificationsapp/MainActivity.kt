0b5f511c542f304d54e8ab14173c10dbec6680b4
Juan Antonio <jcahuanam@gmail.com>
2023-07-04 22:03:39 -0500

    refactor: create initApp fun

--- app/src/main/java/io/juanantonio/pushnotificationsapp/MainActivity.kt
+++ app/src/main/java/io/juanantonio/pushnotificationsapp/MainActivity.kt
@@ -18,15 +18,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PushNotificationsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            initApp();
+        }
+    }
+}
+
+@Composable
+fun initApp(){
+    PushNotificationsAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Init App")
         }
     }
 }
